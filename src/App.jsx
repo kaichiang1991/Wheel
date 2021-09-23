1c4098d0ea67d41046ba29743b365b393368d45c
Kai <kaichiang@starcrown.asia>
2021-09-23 14:53:42 +0800

    修改靜態資源路徑 (homepage) 改用 hashRouter ( 避免轉址錯誤)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import { Route, Switch } from 'react-router'
+import { Redirect, Route, Switch } from 'react-router'
 import Home from './pages/Home'
 import Game from './pages/Game'
 
@@ -22,6 +22,7 @@ export default class App extends Component {
 					<Switch>
 						<Route exact path="/" component={Home}/>
 						<Route path="/game" component={Game}/>
+						<Redirect to={'/'}/>
 					</Switch>
 				</div>
 			</div>
